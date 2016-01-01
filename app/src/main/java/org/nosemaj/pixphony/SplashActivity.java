62462199323bcd986f45537e65c8c39395a805c5
Jameson Williams <jameson.h.williams@intel.com>
2015-12-31 16:04:46 -0800

    ui: clean up the splash screen and fix icon issue
    Signed-off-by: Jameson Williams <jameson.h.williams@intel.com>

--- app/src/main/java/org/nosemaj/pixphony/SplashActivity.java
+++ app/src/main/java/org/nosemaj/pixphony/SplashActivity.java
@@ -40,7 +40,7 @@ public class SplashActivity extends Activity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashActivity.this, DebugActivity.class);
+                Intent mainIntent = new Intent(SplashActivity.this, PianoActivity.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
