a0f79c96c3bfda33ebe2f34f8c10283e678a520a
Jameson Williams <jameson.h.williams@intel.com>
2016-01-11 15:49:50 -0800

    legal: update authorship in pianoactivity
    Signed-off-by: Jameson Williams <jameson.h.williams@intel.com>

--- app/src/main/java/org/nosemaj/pixphony/PianoActivity.java
+++ app/src/main/java/org/nosemaj/pixphony/PianoActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Nagaraj Bhat
+ * Copyright 2015 Jameson Williams, Nagaraj Bhat
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
